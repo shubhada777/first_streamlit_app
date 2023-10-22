5c524e0ca53a7083f9ef7f047a45c729225b7958
shubhada777 <148693908+shubhada777@users.noreply.github.com>
2023-10-22 19:08:40 +0530

    Update streamlit_app.py
    added a special menu item

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
-
+🥣 🥗 🐔 🥑🍞
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('hard-Boiled Free-Range Egg')
-                 
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
+streamlit.header('🍌🥭 Mixed Fruit Smoothie 🥝🍇')
