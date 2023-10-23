a998f490b1b3a366468c2e9960475052f13298fa
shubhada777 <148693908+shubhada777@users.noreply.github.com>
2023-10-23 15:11:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,9 +52,9 @@ except URLError as e:
 streamlit.header("The fruit load list contains:")
 #Snowflake-related functions
 def get_fruit_load_list():
-  with my_cnx.cursor() as my_cur:
-    my_cur.execute("select * from fruit_load_list")
-    return my_cur.fetchall()
+    with my_cnx.cursor() as my_cur:
+      my_cur.execute("select * from fruit_load_list")
+      return my_cur.fetchall()
 
 #add button to load fruit
 if streamlit.button('Get Fruit Load List'):
@@ -66,11 +66,11 @@ if streamlit.button('Get Fruit Load List'):
 
 #Second Text Entry Box
 def insert_row_snowflake(new_fruit):
-  with my_cnx.cursor() as my_cur:
-    my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-    return "Thanks for adding "+new_fruit
+    with my_cnx.cursor() as my_cur:
+      my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+      return "Thanks for adding "+ new_fruit
     
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
   back_from_function=insert_row_snowflake(add_my_fruit)
   streamlit.text(back_from_function)
