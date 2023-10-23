0d7e9932c4576af3e2fafd14eac58503bbca687f
shubhada777 <148693908+shubhada777@users.noreply.github.com>
2023-10-23 12:58:22 +0530

    Update streamlit_app.py
    streamlit query

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,3 +46,5 @@ streamlit.dataframe(my_data_rows)
 #Second Text Entry Box
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
